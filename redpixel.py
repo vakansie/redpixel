6ee45b7a7f731184a181306fb8f4544f8954a9e5
vakansie <91331489+vakansie@users.noreply.github.com>
2021-11-20 00:01:13 +0100

    Add files via upload

--- redpixel.py
+++ redpixel.py
@@ -3,9 +3,10 @@ import numpy
 from PIL import Image, ImageTk #, ImageGrab
 import random
 from time import time
+import math
 
 def unify_vector(vector):
-    if vector[0] == 0 and vector[1] == 0: return vector*0
+    if not numpy.any(vector): return vector
     return vector / (vector**2).sum()**0.5
 
 class Game:
@@ -16,7 +17,6 @@ class Game:
         self.main_canvas.pack(expand=1, fill='both')
         self.player_sprite = ImageTk.PhotoImage(Image.open('redpixel.png'))
         self.hom_sprite = ImageTk.PhotoImage(Image.open('hom1.png'))
-        self.start_time = time()
         self.enemy_spawn_rate = 1
         self.hom_speed_factor = 1
         self.tasks = []
@@ -24,6 +24,7 @@ class Game:
         self.player = None
         self.lost = False
         self.imgnum = iter([str(x).zfill(4) for x in range(500)])
+        self.golden_half = (1 + 5 ** 0.5)
         self.score_display = self.main_canvas.create_text(450, 20, text=f'KILLS:  {0} /25', fill="red", font=('Impact 12 bold'))
         #self.save_canvas()
 
@@ -59,19 +60,22 @@ class Game:
         y = self.main_window.winfo_rooty() + self.main_canvas.winfo_y()
         xx = x + self.main_canvas.winfo_width()
         yy = y + self.main_canvas.winfo_height()
-        ImageGrab.grab(bbox=(x, y, xx, yy)).save(f"images\\redpixel{imgnum}.png")
+        #ImageGrab.grab(bbox=(x, y, xx, yy)).save(f"images\\redpixel{imgnum}.png")
         self.main_window.after(100, self.save_canvas)
 
     def update_score(self):
         self.main_canvas.itemconfig(self.score_display, text=f'KILLS:  {self.player.kill_count:>3} /25')
 
 class Player:
-
     def __init__(self):
         self.pos = numpy.array([20,20], dtype=float)
         self.dist_to_move = numpy.array([0,0], dtype=float)
         self.dir = numpy.array([0,0], dtype=float)
-        self.cooldown = 5
+        self.up_vector = numpy.array([0, -1])
+        self.left_vector = numpy.array([-1, 0])
+        self.down_vector = numpy.array([0, 1])
+        self.right_vector = numpy.array([1, 0])
+        self.cooldown = 0.5
         self.last_shot = 0
         self.moving = None
         self.image = game.main_canvas.create_image(20, 20, image=game.player_sprite)
@@ -84,41 +88,35 @@ class Player:
         now = time()
         if now - self.last_shot > self.cooldown:
             for bullet_num in range(game.player.spread):
-                x, y = mouse_pos.x, mouse_pos.y
-                bullet = Bullet(x,y)
-                game.tasks.append(game.main_canvas.after(30, bullet.move))
-                game.tasks.append(game.main_canvas.after(30, bullet.hit))
+                # if bullet_num == 0:
+                #     Bullet(mouse_pos.x, mouse_pos.y)
+                #     continue
+                if bullet_num % 2 == 0: bullet_num = (bullet_num // 2) * -1
+                x = math.cos((bullet_num/game.golden_half)) * (mouse_pos.x - game.player.pos[0]) - math.sin((bullet_num/game.golden_half)) * (mouse_pos.y - game.player.pos[1]) + game.player.pos[0]
+                y = math.sin((bullet_num/game.golden_half)) * (mouse_pos.x - game.player.pos[0]) + math.cos((bullet_num/game.golden_half)) * (mouse_pos.y - game.player.pos[1]) + game.player.pos[1]
+                Bullet(x, y)
             self.last_shot = now
 
-    def move_player(self, new_direction):
+    def move_player(self, direction_vector):
         if game.lost: return
-        # self.moving.append(new_direction)
-        # if len(self.moving) > 1: self.moving.pop(0)
-        #self.dir *= 0
-        # for direction in self.moving:
-        #     self.dir += direction
-        self.dir = unify_vector(self.dir + unify_vector(new_direction))
+        self.dir = unify_vector(self.dir + unify_vector(direction_vector))
         self.dist_to_move = self.dir * 5
         self.pos += self.dist_to_move
         game.main_canvas.move(self.image, self.dist_to_move[0], self.dist_to_move[1])
+        if self.moving: game.main_canvas.after_cancel(self.moving)
         self.moving = game.main_canvas.after(30, self.move_player, self.dir)
 
-
     def up(self):
-        if self.moving: game.main_canvas.after_cancel(self.moving)
-        self.move_player(numpy.array([0, -1]))
+        self.move_player(self.up_vector)
 
     def left(self):
-        if self.moving: game.main_canvas.after_cancel(self.moving)
-        self.move_player(numpy.array([-1, 0]))
+        self.move_player(self.left_vector)
 
     def down(self):
-        if self.moving: game.main_canvas.after_cancel(self.moving)
-        self.move_player(numpy.array([0, 1]))
+        self.move_player(self.down_vector)
 
     def right(self):
-        if self.moving: game.main_canvas.after_cancel(self.moving)
-        self.move_player(numpy.array([1, 0]))
+        self.move_player(self.right_vector)
 
     def bind_keys(self):
         game.main_window.bind("w", lambda x: self.up())
@@ -156,7 +154,9 @@ class Hom:
         game.main_canvas.delete(self.image)
         del self
         game.player.kill_count += 1
+        game.player.last_shot = 0
         game.update_score()
+        game.player.spread = (game.player.kill_count // 5) + 1
         if game.player.kill_count >= 25:
             game.victory()
             return
@@ -165,9 +165,9 @@ class Hom:
         return
 
 class Bullet:
-    def __init__(self, x, y) -> None:
-        self.dir_from_player = unify_vector(numpy.array([x,y], dtype=float) - game.player.pos)
-        bullet_pos = game.player.pos + (self.dir_from_player * 10)
+    def __init__(self, x, y):
+        self.dir_from_player = unify_vector(numpy.array([x,y]) - game.player.pos)
+        bullet_pos = game.player.pos + (self.dir_from_player * 15)
         self.image = game.main_canvas.create_rectangle(
             bullet_pos[0],
             bullet_pos[1]+6,
@@ -175,12 +175,13 @@ class Bullet:
             bullet_pos[1]+11,
             fill='red',
             tags='bullet')
-        game.main_canvas.after(3000, self.delete)
+        game.tasks.append(game.main_canvas.after(30, self.move))
+        game.tasks.append(game.main_canvas.after(30, self.hit))
+        game.main_canvas.after(2000, self.delete)
 
     def move(self):
         game.main_canvas.move(self.image, self.dir_from_player[0]*10, self.dir_from_player[1]*10)
-        task = game.main_canvas.after(30, self.move)
-        game.tasks.append(task)
+        game.tasks.append(game.main_canvas.after(30, self.move))
 
     def hit(self):
         if 'hit' in game.main_canvas.itemcget(self.image, "tags"):
@@ -188,14 +189,15 @@ class Bullet:
             del self
             return
         hitbox = game.main_canvas.coords(self.image)
-        if hitbox and len(game.main_canvas.find_overlapping(
-            hitbox[0], hitbox[1], hitbox[2], hitbox[3])) != 1:
-            game.main_canvas.addtag_overlapping(
-                'hit',
-                hitbox[0], hitbox[1], hitbox[2], hitbox[3])
-            game.player.last_shot = 0
-        task =game.main_canvas.after(30, self.hit)
-        game.tasks.append(task)
+        if hitbox: 
+            overlap = game.main_canvas.find_overlapping(
+            hitbox[0], hitbox[1], hitbox[2], hitbox[3])
+            if game.player.image not in overlap and len(overlap) != 1:
+                game.main_canvas.addtag_overlapping(
+                    'hit',
+                    hitbox[0], hitbox[1], hitbox[2], hitbox[3])
+                game.player.last_shot = 0
+        game.tasks.append(game.main_canvas.after(30, self.hit))
 
     def delete(self):
         game.main_canvas.delete(self.image)
